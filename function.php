6f307d2643c6b11c1567cc4d7f3bf26b1b2f8e7d
wertyaser <nacmanleonielgarin@gmail.com>
2024-04-25 09:34:23 +0800

    reset

---
+++ function.php
@@ -0,0 +1,16 @@
+<?php
+
+function check_login($conn)
+{
+    if (isset($_SESSION['admin_id'])) {
+        $id = $_SESSION['admin_id'];
+        $query = "select * from admin where user = '$id' limit 1";
+        $result = mysqli_query($conn, $query);
+        if ($result && mysqli_num_rows($result) > 0) {
+            $user_data = mysqli_fetch_assoc($result);
+            return $user_data;
+        }
+    } else {
+        header("Location: error-page.php");
+    }
+}
--- index.php
+++ index.php
@@ -2,27 +2,50 @@
 include ("php/db_connect.php");
 session_start();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Check username and password against the database
-  $username = $_POST["username"];
-  $password = $_POST["password"];
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//   // Check username and password against the database
+//   $username = $_POST["username"];
+//   $password = $_POST["password"];
 
-  $sql = "SELECT * FROM admin WHERE user='$username' AND password='$password'";
-  $result = $conn->query($sql);
+//   $sql = "SELECT * FROM admin WHERE user='$username' AND password='$password'";
+//   $result = $conn->query($sql);
 
-  if ($result->num_rows == 1) {
-    // $_SESSION["username"] = $username;
-    // header("Location: home.php");
-    $row = $result->fetch_assoc();
-    $_SESSION["username"] = $username;
-    $_SESSION["admin_id"] = $row["id"];
-    $_SESSION["type"] = $row["type"];
-    header("Location: home.php");
-    exit();
-  } else {
+//   if ($result->num_rows == 1) {
+//     // $_SESSION["username"] = $username;
+//     // header("Location: home.php");
+//     $row = $result->fetch_assoc();
+//     $_SESSION["username"] = $username;
+//     $_SESSION["admin_id"] = $row["id"];
+//     $_SESSION["type"] = $row["type"];
+//     header("Location: home.php");
+//     exit();
+//   } else {
+//     echo '<script type="text/javascript">alert("Wrong Email or Password") </script>';
+//   }
+
+// }
+
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+  $adminUser = $_POST['adminUser'];
+  $adminPass = $_POST['adminPass'];
+
+  if (!empty($adminUser) && !empty($adminPass)) {
+    $query = "select * from admin where user = '$adminUser' limit 1";
+    $result = mysqli_query($conn, $query);
+
+    if ($result) {
+      $result = mysqli_query($conn, $query);
+      if ($result && mysqli_num_rows($result) > 0) {
+        $user_data = mysqli_fetch_assoc($result);
+        if ($user_data['password'] === $password) {
+          $_SESSION['admin_id'] = $adminUser;
+          header("Location: home.php");
+        }
+      }
+    }
     echo '<script type="text/javascript">alert("Wrong Email or Password") </script>';
   }
-
 }
 ?>
 
@@ -42,8 +65,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <div class="main">
     <h1 class="title">Admin Sign in</h1>
     <form action="" method="post" class="">
-      <input type="text" name="username" aria-label="Text" placeholder="Enter your username" />
-      <input type="password" name="password" placeholder="Enter your password" />
+      <input type="text" name="adminUser" placeholder="Username" />
+      <input type="password" name="adminPass" placeholder="Password" />
       <button type="submit">Submit</button>
     </form>
   </div>
