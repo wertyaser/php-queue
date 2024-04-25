510153d6d718cbd454a82b820d565690b81a2cec
wertyaser <nacmanleonielgarin@gmail.com>
2024-04-25 13:52:22 +0800

    added total of transaction of current user

--- admin.php
+++ admin.php
@@ -5,6 +5,26 @@ include ("function.php");
 check_login($conn);
 $admin_data = check_login($conn);
 $admin_type = $_SESSION['type'];
+
+
+// // Get the current date
+// $current_date = date("Y-m-d");
+
+// // SQL query to count transactions by type for the current date
+// $sql_count_by_type = "SELECT type, COUNT(*) AS count FROM `customers` WHERE date = '$current_date' GROUP BY type";
+// $result_count_by_type = mysqli_query($conn, $sql_count_by_type);
+
+// // Store the counts in an associative array
+// $type_counts = array();
+// while ($row_count_by_type = mysqli_fetch_assoc($result_count_by_type)) {
+//     $type_counts[$row_count_by_type['type']] = $row_count_by_type['count'];
+// }
+
+$sql_count_transactions = "SELECT COUNT(*) AS total_transactions FROM customers WHERE type = '$admin_type' AND DATE(date) = CURDATE()";
+$result_count_transactions = mysqli_query($conn, $sql_count_transactions);
+$row_count_transactions = mysqli_fetch_assoc($result_count_transactions);
+$total_transactions = $row_count_transactions['total_transactions'];
+
 ?>
 
 <!DOCTYPE html>
@@ -21,7 +41,10 @@ $admin_type = $_SESSION['type'];
     <main class="mx-auto w-11/12 max-w-7xl h-full pb-16">
         <div class="flex justify-between pt-24 mb-10">
             <h1 class="text-white font-display text-5xl">Admin</h1>
+            <h1>Total of Transactions(Today): <?php echo $total_transactions ?></h1>
             <div class="flex gap-3">
+                <!-- <h1>Total of Transactions(Today): </h1> -->
+
                 <button class="p-3 bg-yellow-400 text-white rounded-md border border-white font-md shadow-md px-6"><a
                         href="monitoring.php">Open Monitoring</a></button>
                 <button class="p-3 bg-yellow-400 text-white rounded-md border border-white font-md shadow-md px-6"><a
