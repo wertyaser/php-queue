a61745b444145c750b18f7d1a1fd3fbbbc5bc293
wertyaser <nacmanleonielgarin@gmail.com>
2024-05-02 09:19:25 +0800

    alert message for admin login

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         }
       }
     }
-    // echo "You are not an admin.";
+    echo '<script type="text/javascript">alert("Wrong Email or Password") </script>';
   }
 }
 
