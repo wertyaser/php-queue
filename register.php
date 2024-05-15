dd6b46133711160211a791a14162966e99c4beaf
wertyaser <nacmanleonielgarin@gmail.com>
2024-05-15 10:13:46 +0800

    modified:   register.php

--- register.php
+++ register.php
@@ -38,8 +38,6 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="css/output.css">
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <title>Driver/Helper Registration</title>
 </head>
 
