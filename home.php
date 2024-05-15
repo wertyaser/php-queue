15eb51a6c55fb11a8d59c4dfc03db89c5c0a3d56
wertyaser <nacmanleonielgarin@gmail.com>
2024-05-15 11:11:11 +0800

    Changes to be committed: 	modified:   home.php 	new file:   js/sweetalert.min.js

--- home.php
+++ home.php
@@ -130,7 +130,7 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- <link rel="stylesheet" href="./css/pico-main/css/pico.min.css" /> -->
     <link rel="stylesheet" href="css/output.css">
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="js/sweetalert.min.js"></script>
     <script src="js/jquery.min.js"></script>
     <title>Queuing System</title>
 </head>
